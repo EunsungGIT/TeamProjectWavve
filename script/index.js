988ceebe13fa74101e389fe40cafbacd892e44aa
Brave <august8821@naver.com>
2024-02-13 19:02:27 +0900

    메인배너 스와이프

--- script/index.js
+++ script/index.js
@@ -1,14 +1,21 @@
 /* 상단 메인 슬라이드 */
-var mySwiper = new Swiper('#top_container', { 
-    autoplay:{delay:1000},
+const my_swiper = new Swiper('#top_container', { 
+    autoplay:{delay:2000},
     loof:true,
-    slidesPerView: 1,
-    spaceBetween: 10,
+    slidesPerView: 2, //슬라이드 한번에 1개씩 보이기
+    spaceBetween: 20, // 슬라이드 여백
+    centeredSlides:true, // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
+    slidesPerGroup: 1,
+    loop:true,
+    pagination:{
+        el:'.swiper-pagination'
+    },
     navigation:{
         nextEl:'#top_container .swiper-button-next',
         prevEl:'#top_container .swiper-button-prev',
     },
 })
+
 
 /* 하단 슬라이드  */
 var mySwiper = new Swiper('#swiper_wavve_images', { 
