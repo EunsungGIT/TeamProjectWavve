ba9c68af5adea4fde83369611768f3c82789ee0d
Brave <august8821@naver.com>
2024-02-14 19:14:14 +0900

    메인페이지 메인배너 마무리 및 이벤트 종료 선 +

--- script/index.js
+++ script/index.js
@@ -1,11 +1,9 @@
 /* 상단 메인 슬라이드 */
 const my_swiper = new Swiper('#top_container', { 
-    autoplay:{delay:2000},
-    loof:true,
-    slidesPerView: 2, //슬라이드 한번에 1개씩 보이기
+    autoplay:{delay:3000},
+    slidesPerView: 1.5, //슬라이드 한번에 1개씩 보이기
     spaceBetween: 20, // 슬라이드 여백
     centeredSlides:true, // 슬라이드의 기준을 가운데부터 시작하도록 설정하기
-    slidesPerGroup: 1,
     loop:true,
     pagination:{
         el:'.swiper-pagination'
@@ -17,6 +15,26 @@ const my_swiper = new Swiper('#top_container', {
     
 })
 
+/* const activeIndex = document.querySelector(".top_img")
+
+// 슬라이드 활성화 감지 및 중앙 정렬
+my_swiper.on('slideChange', function () {
+    // 활성화된 슬라이드의 인덱스 가져오기
+    activeIndex = my_swiper.realIndex;
+    
+    // 슬라이드가 활성화되면 해당 슬라이드의 너비를 확인하여 뷰포트 중앙에 맞추기
+    var slideWidth = document.querySelector('.swiper-slide-active').clientWidth;
+    var viewportWidth = window.innerWidth;
+
+    // 뷰포트 중앙에 맞추기 위한 이동 거리 계산
+    var moveDistance = (viewportWidth - slideWidth) / 2;
+
+    // 슬라이드 컨테이너를 중앙에 맞추기 위해 CSS 클래스 추가
+    document.querySelector('.swiper-wrapper').style.transform = 'translateX(' + moveDistance + '1240px)';
+});
+
+ */
+
 /* 메인 배너 슬라이더 밑에 멈춤 버튼 실패 작업중 */
 /* const main_stop_btn = document.querySelector(".stop_lockswipes")
 
@@ -25,6 +43,7 @@ main_stop_btn.addEventListener("click", ()=>{
         // Swiper 설정
     });
 })
+
  */
 /* ------------------------------------------------------------ */
 
