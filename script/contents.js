8c42794f04749eb80eabd57f20ad29624b7e11ce
Brave <august8821@naver.com>
2024-02-15 18:18:31 +0900

    메인배너 구매 다운로드 페이지 및 코인 반응형

---
+++ script/contents.js
@@ -0,0 +1,12 @@
+/* 비슷한 영화 및 남녀노소 모두, 오늘은 애니 앞으로 */
+
+var mySwiper = new Swiper('#wavve_images', { 
+    autoplay:{delay:2000},
+    loof:true,
+    slidesPerView: 5,
+    spaceBetween: 10,
+    navigation:{
+        nextEl:'#wavve_images .swiper-button-next',
+        prevEl:'#wavve_images .swiper-button-prev',
+    },
+})
