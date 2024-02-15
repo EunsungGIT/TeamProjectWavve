51c66a015a9f940076d0c3c6e89bc674777c64b0
EunsungGIT <spacerdt@gmail.com>
2024-02-15 17:03:42 +0900

    이용권 태그 수정

---
+++ script/live_contents.js
@@ -0,0 +1,7 @@
+var mySwiper = new Swiper('#swiper_schedule', { 
+    slidesPerView: 4,
+    navigation:{
+        nextEl:'#swiper_schedule #schedule_next',
+        prevEl:'#swiper_schedule #schedule_prev',
+    },
+})
--- script/search.js
+++ script/search.js
@@ -1,8 +1,24 @@
 var mySwiper = new Swiper('#swiper_series', { 
-    slidesPerView: 5,
+    slidesPerView: 3,
     spaceBetween: 10,
+    centeredSlides : false,
+    loop:true,
     navigation:{
         nextEl:'#swiper_series #search_next',
         prevEl:'#swiper_series #search_prev',
+    },
+    breakpoints: {
+        768: {
+            slidesPerView: 4,
+            spaceBetween: 10,
+            centeredSlides : true,
+            loop:true,
+        },
+        1240: {
+            slidesPerView: 5,
+            spaceBetween: 10,
+            centeredSlides : true,
+            loop:true,
+        }
     },
 })
