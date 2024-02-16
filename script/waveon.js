1b2e3be7cbd082fd6c4b2cfc45644ede7efa8ff1
leejonghyunbeen <clfl19@naver.com>
2024-02-16 18:25:25 +0900

    회원가입 페이지

---
+++ script/waveon.js
@@ -0,0 +1,6 @@
+const join = document.querySelector( '#join')
+const waveon = document.querySelector( '#interests')
+console.log(join,waveon)
+join.addEventListener('click',()=>{
+    alert('본인 인증 페이지로 이동합니다')
+})
