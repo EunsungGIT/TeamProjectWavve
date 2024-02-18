c525ec7fe9d5cb3fcb85c52caf6841fffe94f32c
EunsungGIT <spacerdt@gmail.com>
2024-02-18 20:03:26 +0900

    공지사항 js 작업중

--- script/live_contents.js
+++ script/live_contents.js
@@ -1,3 +1,27 @@
+// 팝업 비디오
+const popup_video = document.querySelector('.popup_video')
+const main_video = document.querySelector('.video_bg .video')
+const closeBtn = document.querySelector('.popup_video > a')
+console.log(popup_video,main_video)
+
+popup_video.style.display = 'none'
+window.addEventListener('scroll',()=>{
+    const scrollPosition = window.scrollY;
+    console.log(scrollPosition)
+
+    if(scrollPosition >= 300){
+        popup_video.style.display = 'flex'
+        main_video.style.display = 'none'
+    }else{
+        popup_video.style.display = 'none'
+        main_video.style.display = 'block'
+    }
+})
+
+// 메인 비디오
+
+
+// 스케줄 swiper
 var mySwiper = new Swiper('#swiper_schedule', { 
     slidesPerView: 4,
     navigation:{
