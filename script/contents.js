26c4e543cf06a15ca81908a4915cb96ca7f4e372
leejonghyunbeen <clfl19@naver.com>
2024-02-19 18:08:56 +0900

    로그인 자바끝

--- script/contents.js
+++ script/contents.js
@@ -8,3 +8,34 @@ var mySwiper = new Swiper('#wavve_images', {
         prevEl:'#wavve_images .swiper-button-prev',
     },
 })
+
+//일반자바
+const related_video = document.querySelector('.related_video')
+const recommendation = document.querySelector('.recommendation')
+const details =document.querySelector('.details')
+
+const info_contents =document.querySelector('.info_contents')
+const recommend =document.querySelector('.recommend')
+const information =document.querySelector('.information')
+console.log(related_video,recommendation,details)
+console.log(info_contents,recommend,information)
+recommend.style.display = 'none'
+information.style.display = 'none'
+
+related_video.addEventListener('click',()=>{
+    recommend.style.display = 'none'
+    information.style.display = 'none'
+    info_contents.style.display='block'
+
+})
+
+recommendation.addEventListener('click',()=>{
+    recommend.style.display = 'block'
+    information.style.display='none'
+    info_contents.style.display ='none'
+})
+details.addEventListener('click',()=>{
+    info_contents.style.display ='none'
+    recommend.style.display = 'none'
+    information.style.display = 'block'
+})
