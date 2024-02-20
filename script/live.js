0ae4622cc2de5cd19b6b74eea5850208e2b4fa76
EunsungGIT <spacerdt@gmail.com>
2024-02-20 11:03:06 +0900

    라이브 js

--- script/live.js
+++ script/live.js
@@ -1,3 +1,18 @@
+/* 시청예약 */
+const reservation_table = document.querySelectorAll('.reservation_table a')
+const reservation_bg = document.querySelector('.reservation_bg')
+const reservation_close = document.querySelector('.reservation #close')
+
+reservation_bg.style.display = 'none'
+reservation_table[0].addEventListener('click',()=>{
+    reservation_bg.classList.add('wide')
+    reservation_bg.style.display = 'flex'
+})
+reservation_close.addEventListener('click',()=>{
+    reservation_bg.style.display = 'none'
+})
+
+/* 비디오 */
 const live_video_a = document.querySelectorAll('.live_video a')
 const live_video = document.querySelectorAll('.live_video a video')
 console.log(live_video,live_video_a)
