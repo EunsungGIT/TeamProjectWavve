6bc8056d89f2465e5517e406a39fafcc9d52edb9
Brave <august8821@naver.com>
2024-02-21 18:05:54 +0900

    알림함 작업

--- script/holding_coin.js
+++ script/holding_coin.js
@@ -1,13 +1,7 @@
 const holding_coin_btn = document.querySelector(".holding_middle_title > a")
 
-/* <script>
-document.getElementById("popupButton").addEventListener("click", function() {
-  window.open("popup.html", "popup", "width=600,height=400"); // 팝업창 크기 및 설정
-});
-</script> */
 
 holding_coin_btn.addEventListener("click", ()=>{
     window.open("coin_charging_page.html", "popup", "width=699,height=1377")
 })
 
-/* document.getElementById("holding_coin_btn") */
--- script/mypage.js
+++ script/mypage.js
@@ -1,3 +1,17 @@
 /* 알림함 */
 const notification = document.querySelector('.notification')
 const contents_all = document.querySelector('.contents_all')
+
+const notification_btn = document.querySelector(".mypage_top a:nth-child(0)")
+console.log(notification_btn)
+
+notification_btn.addEventListener("click", ()=>{
+    window.open("mypage_notification_box.html", "popup", "width=560, height=550")
+})
+
+
+/* "width=560,height=550" */
+/* holding_coin_btn.addEventListener("click", ()=>{
+    window.open("coin_charging_page.html", "popup", "width=699,height=1377")
+})
+ */
