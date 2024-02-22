603912c8a1b4e864914b5355f6a2a351f9a06651
EunsungGIT <spacerdt@gmail.com>
2024-02-22 16:10:21 +0900

    알림함 메인페이지 이동

--- script/common.js
+++ script/common.js
@@ -65,12 +65,49 @@ profile.addEventListener('mouseleave', () => {
 
 // header 프로필 내 알림함
 const profile_m_a = document.querySelectorAll('.profile_m a')
-const notification = document.querySelector('.notification')
-const notification_box = document.querySelector('.notification_box')
+const notification_entire = document.querySelector(".notification_bg")
+const notifiaction_wrapper = document.querySelector('.notification_wrap')
+const notification_wrap = document.querySelector(".notification")
+const setting_wrap = document.querySelector(".setting_wrap")
 
-// notification.style.display = 'none';
-profile_m_a[3].addEventListener('click', ()=>{
-    notification.style.display = 'block';
+notification_entire.style.display = 'none'
+profile_m_a[3].addEventListener('click',()=>{
+    body_html.style.overflow = 'hidden'
+    notification_entire.style.display = 'block'
+    notifiaction_wrapper.classList.add('wide')
+})
+
+/* 알림함 & 알림설정 */
+
+/* 알림함 & 알림설정 버튼 - > 알림설정으로 가는 버튼 */
+const notification_btn = document.querySelector(".notification_top a")
+console.log(notification_btn)
+const notification_title = document.querySelector('.notification_top h2')
+const notification_title_btn = document.querySelector("notification_top a img")
+const notification_close = document.querySelector(".setting_close > a")
+console.log(notification_title,notification_title_btn )
+/* 초기 내용 숨기기 */
+setting_wrap.style.display = "none"
+notification_btn.parentElement.classList.add("active")
+
+notification_btn.addEventListener("click", () => {
+    if (setting_wrap.style.display === "none") {
+        setting_wrap.style.display = "block";
+        notification_wrap.style.display = "none";
+        notification_title.innerHTML = '알림설정'
+        notification_btn.innerHTML = "<img src= /images/icon/notification_icon.png> 알림함"
+    } else {
+        setting_wrap.style.display = "none";
+        notification_wrap.style.display = "block";
+        notification_title.innerHTML = '알림함';
+        notification_btn.innerHTML = "<img src= /images/icon/notification_icon.png> 알림설정";
+    }
+});
+
+/* 알림함 x 버튼 */
+notification_close.addEventListener("click", ()=>{
+    body_html.style.overflow = 'auto'
+    notification_entire.style.display = "none"
 })
 
 // header 쿠폰 mouseenter
