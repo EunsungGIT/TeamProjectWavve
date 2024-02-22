60917767e7cbb4f5d4ebdc4f23e6675326d9bed3
Brave <august8821@naver.com>
2024-02-22 18:19:50 +0900

    코인충전 완료

--- script/coin_charging.js
+++ script/coin_charging.js
@@ -1,4 +1,4 @@
-/* 탭------------------------------------------------------------ */
+/* 탭 */
 const normal_tap = document.querySelector(".tab_nomal")
 const auto_tap = document.querySelector(".tab_auto")
 
@@ -58,7 +58,7 @@ charging_tap[1].addEventListener("click", ()=>{
     normal_charging_tap.style.display = "none"
 })
 
-/* 빠른 결제 & 다른 결제 수단  작업중 ---------------------------------------------*/
+/* 빠른 결제 & 다른 결제 수단  작업중*/
 /* card_pay_ment_box 빠른결제 아래로 전부 */
 /* card_btm_content 다른 결제수단 아래로 전부 */
 
@@ -76,26 +76,21 @@ console.log(other_pay_btn)
 
 /* 초기 내용 숨기기 */
 different_payment_box.style.display= "none"
-
 quick_pay_btn.parentElement.classList.add("active")
 
 
 other_pay_btn.addEventListener("click", ()=> {
-    console.log("성공")
     quick_payment_box.style.display ="none"
     different_payment_box.style.display = "flex"
 })
 
 
 quick_pay_btn.addEventListener("click", ()=> {
-    console.log("성공")
     different_payment_box.style.display = "none"
     quick_payment_box.style.display ="flex"
 })
-/* 일반충전 끝 --------------------------------------------------------------------*/
-
-/* 자동충전 시작------------------------------------------------------------------- */
-
+/* 일반충전 끝*/
+/* 자동충전 시작*/
 
 /* 빠른 결제 박스 */
 const auto_quick_payment_box = document.querySelector("#frm_payment2 .card_pay_ment_box")
@@ -112,20 +107,17 @@ auto_different_payment_box.style.display = "none"
 auto_quick_pay_btn.classList.add("active")
 
 auto_other_pay_btn.addEventListener("click", ()=> {
-    console.log("dddddd")
     auto_quick_payment_box.style.display = "none"
     auto_different_payment_box.style.display = "flex"
 })
 
 auto_quick_pay_btn.addEventListener("click",()=>{
-    console.log("dddddd")
     auto_different_payment_box.style.display = "none"
     auto_quick_payment_box.style.display = "flex"
 })
 
-
 /* 자동충전 끝 */
-/* 빠른 결제 & 다른 결제 수단 끝 -------------------------------------------------------*/
+/* 빠른 결제 & 다른 결제 수단 끝 */
 
 /* 카드등록 */
 const card_register = document.querySelector(".card_wrap > .card_register_img")
@@ -136,37 +128,27 @@ card_register.addEventListener("click", ()=>{
 })
 
 /* 일반충전 & 자동충전 화면 전환  끝*/
-
 /* 결제내용- > 충전코인리스트 */
-
+/* 결과금액 */
 const coin_price = [3000, 5000, 10000, 30000]
 const coin_price_span_nomal = document.querySelectorAll("#frm_payment1 .charging_coin_list span")
 const coin_price_span_auto = document.querySelectorAll("#frm_payment2 .charging_coin_list span")
-/* 결과금액 */
 
-/* 일반충전 */
+/* 일반충전 - 충전 코인 선택 */
 const nomal_coin_result = document.querySelector("#frm_payment1 .price")
-
-console.log(nomal_coin_result)
-console.log(coin_price_span_nomal,coin_price_span_auto)
 
 coin_price_span_nomal.forEach((t,i)=>{
     console.log(t,i)
     t.addEventListener("click", ()=>{
-        /* span 클릭 시  */
         console.log(i)
         console.log(coin_price[i])
-        coin_price[i]
-/*         nomal_coin_result */
-       /*  getElementById *//* ("coin_price_span_nomal").innerHTML="dddd" */   
+        coin_price[i]   
         nomal_coin_result.innerHTML = coin_price[i].toLocaleString("ko-kr")
     })
 })
 
-
-/* 자동충전 */
+/* 자동충전  - 충전 코인 선택 */
 const auto_coin_result = document.querySelector("#frm_payment2 .price")
-    console.log(auto_coin_result)
 
 coin_price_span_auto.forEach((t,i)=>{
     t.addEventListener("click", ()=> {
@@ -176,54 +158,57 @@ coin_price_span_auto.forEach((t,i)=>{
     })
 })
 
+/* 일반충전 - 다른 결제 수단 */
+const card_btm_content_box_normal = document.querySelectorAll("#frm_payment1 .card_btm_content li > a");
+const coin_abox_img_normal = document.querySelectorAll("#frm_payment1 .card_btm_content a > img")
+const coin_abox_span_normal = document.querySelectorAll("#frm_payment1 .card_btm_content a > span")
 
-/* 
-menu_title.forEach((t,i)=>{
-    console.log(t,i)
-    t.addEventListener('click',()=>{
-        border_remove()
-        t.classList.add('contants_border')
-        hide()
-        contents_container[i].style.display = 'block'
+/* 자동충전 - 다른 결제 수단 */
+const card_btm_content_box_auto = document.querySelectorAll("#frm_payment2 .card_btm_content li > a");
+const coin_abox_img_auto = document.querySelectorAll("#frm_payment2 .card_btm_content a > img")
+const coin_abox_span_auto = document.querySelectorAll("#frm_payment2 .card_btm_content a > span")
+
+/* 일반&자동 충전 다른 결제 수단 초기화 함수*/
+const card_normal_auto_reset = () => {
+    for(let i of card_btm_content_box_normal){
+        i.classList.remove("card_btm_content_box_style");
+        i.style.background = "initial";
+        i.style.color = "initial";
+        i.style.border = ""
+    }
+    for(let i of card_btm_content_box_auto){
+        i.classList.remove("card_btm_content_box_style");
+        i.style.background = "initial";
+        i.style.color = "initial";
+        i.style.border = ""
+    }
+    for(let i of coin_abox_img_normal){i.style.filter="brightness(100%)"}
+    for(let i of coin_abox_img_auto){i.style.filter="brightness(100%)"}
+    for(let i of coin_abox_span_normal){i.style.color="#a5a5a5"}
+    for(let i of coin_abox_span_auto){i.style.color="#a5a5a5"}
+}
+
+card_normal_auto_reset()
+
+//일반 충전 a 클릭 시 이벤트
+card_btm_content_box_normal.forEach((t,i)=>{
+    t.addEventListener('click',(e)=>{
+        e.preventDefault()
+        card_normal_auto_reset()
+        t.style.background = "#1351f9";
+        t.style.border = "1px solid #1351f9";
+        coin_abox_img_normal[i].style.filter="brightness(1200%)"
+        coin_abox_span_normal[i].style.color="#FFF"
     })
 })
-*/
-
-/* 다른결제 수단 blue 클릭 이벤트 */
-
-const card_btm_content_box = document.querySelectorAll(".card_btm_content li > a");
-/* 이미지 변경 변수 생성 */
-const coin_abox_img = document.querySelector("card_btm_content a > img")
-console.log(card_btm_content_box,coin_abox_img)
-
-// card_btm_content_box.forEach((t,i)=>{
-//     console.log(t,i)
-//     t.addEventListener("click",()=>{
-//         t.classList.add("card_btm_content_box_style")
-//         card_btm_content_box[i].style.background = "#1351f9"
-//         card_btm_content_box[i].style.color = "#fff"
-//         card_btm_content_box[i].style.border = "#1351f9"
-//     })
-// })
-
-card_btm_content_box.forEach((t, i) => {
-    t.addEventListener("click", () => {
-        // 클릭되지 않은 박스에 대한 스타일 초기화
-        card_btm_content_box.forEach(coin_abox => {
-            if (coin_abox !== t) { // 클릭된 박스 아닌 경우에만 초기화
-                coin_abox.classList.remove("card_btm_content_box_style");
-                coin_abox.style.background = "initial";
-                coin_abox.style.color = "initial";
-                // 보더 스타일은 초기화하지 않음
-            }
-        });
-
-        // 클릭한 박스에 스타일 적용
-        t.classList.add("card_btm_content_box_style");
+//자동 충전 a 클릭 시 이벤트
+card_btm_content_box_auto.forEach((t,i)=>{
+    t.addEventListener('click',(e)=>{
+        e.preventDefault()
+        card_normal_auto_reset()
         t.style.background = "#1351f9";
-        t.style.color = "#fff";
-        t.style.border = "1px solid #5e5e5e"; // 클릭한 요소의 border 스타일 설정
-        event.preventDefault(); // 위로 올라가는 동작을 방지해준다.
-        coin_abox_img[i].src = "./images/holding_coin/coin_icon-card-on.png";
-    });
-});
+        t.style.border = "1px solid #1351f9";
+        coin_abox_img_auto[i].style.filter="brightness(1200%)"
+        coin_abox_span_auto[i].style.color="#FFF"
+    })
+})
