7ca0f4772d3e4c6997610bebea08fd6a9011ac1c
unknown <clfl19@naver.com>
2024-02-25 20:02:42 +0900

    멤버쉽 자바스크립트 동적기능 및 디자인 수정완료

--- script/membership.js
+++ script/membership.js
@@ -11,3 +11,30 @@ warning[3].addEventListener('click',(e)=>{
     window.location.href = './mypage.html'
     e.preventDefault();
 })
+const open_btn = document.querySelector('.open')
+const event_img = document.querySelector('.event')
+console.log (event_img)
+let turn_function = true
+open_btn.addEventListener('click',(e)=>{
+    turn_function = !turn_function
+    console.log(turn_function)
+    let rotate_deg = turn_function ? 0 :180;
+    open_btn.style.transform = 'rotate('+rotate_deg + 'deg)'
+    if(turn_function ===false){
+        event_img.style.height = '503px'
+       
+    }else{
+        event_img.style.height = '110px'
+        
+    }
+    e.preventDefault()
+})
+
+const pay_btn = document.querySelectorAll('body button')
+
+for(let y of pay_btn){
+    y.addEventListener('click',(e)=>{
+        window.open("payment.html", "popup", "width=699,height=1377")
+        e.preventDefault()
+    })
+}
